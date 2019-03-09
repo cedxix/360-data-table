c11154c1cab5cfd5403bd368867a7e5102da22c3
Cedric D. ANIDIE <cedric.anidie@consultant.bva-group.com>
2019-03-09 23:08:08 +0100

    column with units dropdown

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -8,13 +8,38 @@ import { chain, get, map } from 'lodash';
 })
 export class DataTableComponent implements OnInit {
 
-  rows = [];
-  columns = [];
   handlingUnits = [];
 
-  stateTable = {
-    handlingUnits: {}
-  };
+  weightUnits = [
+    {
+      value: 'Pounds',
+      label: 'lb'
+    }, {
+      value: 'Kilograms',
+      label: 'kg'
+    }
+  ];
+  volumeUnits = [
+    {
+      value: 'Cubic CM',
+      label: 'cm3'
+    }, {
+      value: 'Cubic Foot',
+      label: 'ft3'
+    }
+  ];
+  lengthUnits = [
+    {
+      value: 'Inches',
+      label: 'In'
+    }
+  ];
+  densityUnits = [
+    {
+      value: 'GM/CC',
+      label: 'gm/cc'
+    }
+  ];
 
   constructor() {
   }
@@ -28,53 +53,47 @@ export class DataTableComponent implements OnInit {
 
 
   prepareData(data: any = []): object[] {
-
-    const handlingColumns = [
-      {
-        prop: 'pickupAt',
-        name: 'Pickup At'
-      }, {
-        prop: 'deliveryAt',
-        name: 'Delivery At'
-      }, {
-        prop: 'handlingUnit',
-        name: 'Handling Unit'
-      }, {
-        prop: 'count',
-        name: 'Count'
-      }, {
-        prop: 'weight',
-        name: 'Weight'
-      }, {
-        prop: 'volume',
-        name: 'Volume'
-      }, {
-        prop: 'length',
-        name: 'Length'
-      }, {
-        prop: 'width',
-        name: 'Width'
-      }, {
-        prop: 'height',
-        name: 'Height'
-      }, {
-        prop: 'density',
-        name: 'Density'
-      }
-    ];
     const {stopItemHandlingDetailAssociation = []} = data;
 
     return stopItemHandlingDetailAssociation.map((handlingItem) => ({
-      pickupAt: get(handlingItem, 'pickupAtSeq'),
-      deliveryAt: get(handlingItem, 'deliveryAtSeq'),
+      pickupAt: [
+        {
+          label: get(handlingItem, 'pickupAtSeq'),
+          value: get(handlingItem, 'pickupAtSeqId')
+        }, {
+          label: '45',
+          value: 45
+        }
+      ],
+      deliveryAt: [
+        {
+          label: get(handlingItem, 'deliveryAtSeq'),
+          value: get(handlingItem, 'deliveryAtSeqId')
+        }, {
+          label: '45',
+          value: 45
+        }
+      ],
       handlingUnit: get(handlingItem, 'itemHandlingDetail.itemHandlingTypeCode'),
       count: get(handlingItem, 'itemHandlingDetail.itemHandlingTypeQuantity'),
-      weight: get(handlingItem, 'itemHandlingDetail.itemHandlingUnitWeight'),
-      volume: get(handlingItem, 'itemHandlingDetail.itemHandlingUnitVolume'),
-      length: get(handlingItem, 'itemHandlingDetail.itemHandlingUnitLength'),
+      weight: {
+        label: get(handlingItem, 'itemHandlingDetail.itemHandlingUnitWeight'),
+        unit: get(handlingItem, 'itemHandlingDetail.unitOfWeightMeasurementCode')
+      },
+      volume: {
+        label: get(handlingItem, 'itemHandlingDetail.itemHandlingUnitVolume'),
+        unit: get(handlingItem, 'itemHandlingDetail.unitOfVolumeMeasurementCode')
+      },
+      length: {
+        label: get(handlingItem, 'itemHandlingDetail.itemHandlingUnitLength'),
+        unit: get(handlingItem, 'itemHandlingDetail.unitOfLengthMeasurementCode')
+      },
       width: get(handlingItem, 'itemHandlingDetail.itemWidth'),
       height: get(handlingItem, 'itemHandlingDetail.itemHeight'),
-      density: get(handlingItem, 'itemHandlingDetail.itemHandlingUnitLength'),
+      density: {
+        label: get(handlingItem, 'itemHandlingDetail.itemHandlingUnitLength'),
+        unit: get(handlingItem, 'itemHandlingDetail.unitOfDensityMeasurementCode')
+      },
       stopItems: get(handlingItem, 'itemHandlingDetail.stopItems')
     })).map(({stopItems = null, ...row}) => ({
       rows: [row],
@@ -82,15 +101,30 @@ export class DataTableComponent implements OnInit {
         rows: stopItems.map((item) => ({
           description: get(item, 'itemDescription'),
           reference: get(item, 'stopItemReferenceNumberAssociations'),
-          referenceType: get(item, 'stopReferenceNumber.referenceNumberTypeCode'),
+          referenceType: {
+            label: get(item, 'stopReferenceNumber.referenceNumberTypeCode'),
+            value: get(item, 'stopReferenceNumber.referenceNumberTypeCode')
+          },
           packageType: get(item, 'packagingUnitTypeCode'),
           count: get(item, 'packagingUnitTypeQuantity'),
-          weight: get(item, 'itemWeight'),
-          volume: get(item, 'itemVolume'),
-          length: get(item, 'itemLength'),
+          weight: {
+            label: get(item, 'itemWeight'),
+            unit: get(item, 'unitOfWeightMeasurementCode')
+          },
+          volume: {
+            label: get(item, 'itemVolume'),
+            unit: get(item, 'unitOfVolumeMeasurementCode')
+          },
+          length: {
+            label: get(item, 'itemLength'),
+            unit: get(item, 'unitOfLengthMeasurementCode')
+          },
           width: get(item, 'itemWidth'),
           height: get(item, 'itemHeight'),
-          density: get(item, 'itemDensity'),
+          density: {
+            label: get(item, 'itemDensity'),
+            unit: get(item, 'unitOfDensityMeasurementCode')
+          }
         }))
       }) : null
     }));
